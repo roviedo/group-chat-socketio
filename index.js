a86349bd4bebab9051aac7816a87fee054953a9e
Raul Oviedo <antoniorah@gmail.com>
2019-11-10 16:45:39 -0500

    Ability to add user to users array and created a button to click to show users

--- index.js
+++ index.js
@@ -6,22 +6,34 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
+var users = [];
 
 io.emit('some event', { for: 'everyone' });
 
-io.on('connection', function(socket){
-  socket.broadcast.emit('hi');
-});
 
 io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.broadcast.emit('hi');
+
   socket.on('chat message', function(obj){
+    console.log('users', users);
     io.emit('chat message', obj);
+  });
+
+  socket.on('add user', function(userName) {
+    socket.userName = userName;
+    users.push(userName);
+    io.emit('add user', userName);
+
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      users: users
+    });
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
   });
 });
 
